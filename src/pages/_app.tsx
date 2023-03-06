d158655291efd2dbebb91297df3a9103d4fa7086
Javier A Mora gil <j4viermora@gmail.com>
2023-03-05 20:29:30 -0400

    basic config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,35 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { extendTheme } from "@chakra-ui/react";
+import AdminLayout from "@/layouts/Admin";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout =
+    Component.getLayout ?? ((page) => <AdminLayout>{page}</AdminLayout>);
+  return (
+    <ChakraProvider theme={theme}>
+      {getLayout(<Component {...pageProps} />)}
+    </ChakraProvider>
+  );
 }
---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Head from "next/head";
+
+const Admin = () => {
+  return (
+    <>
+      <Head>
+        <title>Evernote Clone</title>
+        <meta name="description" content="This is an Evernote Clone" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section>
+        <h1>Admin</h1>
+      </section>
+    </>
+  );
+};
+
+export default Admin;
